f84152a63cc9f0c66e0a1541d445950598b2c15d
Anapah <70201574+reactxsw@users.noreply.github.com>
2022-01-21 16:44:13 +0700

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-redeem("0614097733" , "https://gift.truemoney.com/campaign/?v=BgOnsnox4d85q1oBbj").then(value => {
-    console.log(`RECEIVED ${value.AMOUNT} FROM ${value.SENDER}`);
-  }).catch(error => {
-    console.error(`${error}`)
-})
